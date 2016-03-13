c4735fb4df847eb47d5dd54eee2c116c7270496d
Kriyss <perso@kriyss.ninja>
2016-03-13 16:23:50 +0100

    test: accept method

--- server/server_test.go
+++ server/server_test.go
@@ -1,13 +1,49 @@
 package main
 
-/*
-func TestAccept(t *testing.T) {
-	connections := make(chan net.Conn)
-	ln, _ := net.Listen("tcp", ":3000")
-	accept(ln, connections)
+import (
+	"net"
+	"testing"
+	"time"
+)
 
-	if len(connections) == 0 {
+func TestAccept(t *testing.T) {
+	var conn net.Conn
+	done := make(chan bool)
+	connections := make(chan net.Conn)
+
+	go func() {
+		conn = <-connections
+		done <- true
+	}()
+	go accept(&stubListener{}, connections)
+	<-done
+
+	if conn.RemoteAddr().String() != "1.2.3.4:5" {
 		t.Fail()
 	}
 }
-*/
+
+type stubListener struct {
+}
+
+func (m stubListener) Accept() (net.Conn, error) { return stubConn{}, nil }
+func (m stubListener) Close() error              { return nil }
+func (m stubListener) Addr() net.Addr            { return nil }
+
+type stubConn struct {
+}
+
+func (m stubConn) RemoteAddr() net.Addr               { return stubAddr{} }
+func (m stubConn) Write(b []byte) (n int, err error)  { return 0, nil }
+func (m stubConn) SetDeadline(t time.Time) error      { return nil }
+func (m stubConn) SetReadDeadline(t time.Time) error  { return nil }
+func (m stubConn) SetWriteDeadline(t time.Time) error { return nil }
+func (m stubConn) Read(b []byte) (n int, err error)   { return 0, nil }
+func (m stubConn) LocalAddr() net.Addr                { return nil }
+func (m stubConn) Close() error                       { return nil }
+
+type stubAddr struct {
+}
+
+func (m stubAddr) Network() string { return "network" }
+func (m stubAddr) String() string  { return "1.2.3.4:5" }
