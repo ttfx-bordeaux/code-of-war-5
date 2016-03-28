0ef445488547ebdc29a35aee71efc42f3cea6d33
Kriyss <perso@kriyss.ninja>
2016-03-28 16:49:08 +0200

    refator: remove useless parameters in tests

--- server/game/game_test.go
+++ server/game/game_test.go
@@ -22,16 +22,13 @@ func TestJoinGame(t *testing.T) {
 }
 
 func TestQuitGame(t *testing.T) {
-	c1 := Client{ID: "1", Name: "name1"}
-	c3 := Client{ID: "3", Name: "name3"}
-
 	game, _ := NewGame("name")
-	game.Join(c1)
+	game.Join(Client{ID: "1", Name: "name1"})
 	game.Join(Client{ID: "2", Name: "name2"})
-	game.Join(c3)
+	game.Join(Client{ID: "3", Name: "name3"})
 
-	game.Remove(c1)
-	game.Remove(c3)
+	game.Remove(Client{ID: "1"})
+	game.Remove(Client{ID: "3"})
 
 	if len(game.Players) != 1 || game.Players["2"].Name != "name2" {
 		t.Fail()
