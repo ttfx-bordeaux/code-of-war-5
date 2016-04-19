87957c3275c60aa2ed73a0ee01a59bcff5802574
sebastienD <sebastien.descamps@gmail.com>
2016-04-19 23:43:15 +0200

    fix: add missing files

---
+++ engine/game/system.go
@@ -0,0 +1,48 @@
+package system
+
+import (
+	"engo.io/ecs"
+	"engo.io/engo"
+)
+
+var (
+	AnimationRate float32 = 0.1
+	WalkAction    *engo.AnimationAction
+	StopAction    *engo.AnimationAction
+	DieAction     *engo.AnimationAction
+	Actions       []*engo.AnimationAction
+)
+
+func Preload() {
+	// animation for chicken
+	StopAction = &engo.AnimationAction{Name: "stop", Frames: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}}
+	WalkAction = &engo.AnimationAction{Name: "move", Frames: []int{11, 12, 13, 14, 15}}
+	DieAction = &engo.AnimationAction{Name: "die", Frames: []int{28, 29, 30}}
+	Actions = []*engo.AnimationAction{DieAction, StopAction, WalkAction}
+}
+
+type ControlSystem struct {
+	ecs.LinearSystem
+}
+
+func (*ControlSystem) Type() string { return "ControlSystem" }
+func (*ControlSystem) Pre()         {}
+func (*ControlSystem) Post()        {}
+
+func (c *ControlSystem) New(*ecs.World) {}
+
+func (c *ControlSystem) UpdateEntity(entity *ecs.Entity, dt float32) {
+	var a *engo.AnimationComponent
+
+	if !entity.Component(&a) {
+		return
+	}
+
+	if engo.Keys.Get(engo.ArrowRight).Down() {
+		a.SelectAnimationByAction(WalkAction)
+	} else if engo.Keys.Get(engo.Space).Down() {
+		a.SelectAnimationByAction(DieAction)
+	} else {
+		a.SelectAnimationByAction(StopAction)
+	}
+}
