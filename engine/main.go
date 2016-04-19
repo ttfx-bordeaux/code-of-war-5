1a90026897eec9edf99d707133094b2dfbc355b3
sebastienD <sebastien.descamps@gmail.com>
2016-04-19 23:03:53 +0200

    feat: add multiple grounds

--- engine/main.go
+++ engine/main.go
@@ -6,17 +6,39 @@ import (
 
 	"engo.io/ecs"
 	"engo.io/engo"
+	"github.com/ttfx-bordeaux/code-of-war-5/engine/game"
 )
 
 var (
-	TileWidth float32
-	TileHeight float32
-	animationRate float32
-	WalkAction *engo.AnimationAction
-	StopAction *engo.AnimationAction
-	DieAction  *engo.AnimationAction
-	actions    []*engo.AnimationAction
+	screenWidth                 = 1024
+	screenHeight                = 640
+	TileWidth       int         = 120
+	TileHeight      int         = 120
+	nbTilesAbs      int         = 3
+	nbTilesOrd      int         = 5
+	ImgGroundName   string      = "grass-600-600.png"
+	ImgTowerName    string      = "tour1-600-600.png"
+	backgroundColor color.Color = color.White
+	padRight        int
+	players         map[string]*Player
 )
+
+type GamePosition struct {
+	Abs int
+	Ord int
+}
+
+func (p GamePosition) toPoint(loop int) engo.Point {
+	padding := loop * (nbTilesAbs*TileWidth + padRight)
+	return engo.Point{X: float32(p.Abs*TileWidth + padding), Y: float32(p.Ord * TileHeight)}
+}
+
+type Player struct {
+	Id       string
+	Ground   []*ecs.Entity
+	towers   []*ecs.Entity
+	chickens []*ecs.Entity
+}
 
 //GameWorld world
 type GameWorld struct{}
@@ -26,46 +48,47 @@ func (game *GameWorld) Preload() {
 	// Load all files from the data directory. Do not do it recursively.
 	engo.Files.AddFromDir("static", false)
 
-	//engo.Files.Add("static/chicken.png")
-	// animation for chicken
-	StopAction = &engo.AnimationAction{Name: "stop", Frames: []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}}
-	WalkAction = &engo.AnimationAction{Name: "move", Frames: []int{11, 12, 13, 14, 15}}
-	DieAction = &engo.AnimationAction{Name: "die", Frames: []int{28, 29, 30}}
-	actions = []*engo.AnimationAction{DieAction, StopAction, WalkAction}
-	
+	system.Preload()
+
 	log.Println("Preloaded")
 }
 
 //Setup setup
 func (game *GameWorld) Setup(w *ecs.World) {
-	engo.SetBackground(color.White)
+	engo.SetBackground(backgroundColor)
 
 	w.AddSystem(&engo.RenderSystem{})
 	w.AddSystem(&engo.AnimationSystem{})
-	w.AddSystem(&ControlSystem{})
-	
+	w.AddSystem(&system.ControlSystem{})
+
+	loop := 0
+	for _, p := range players {
+		log.Println(p)
+		log.Println(loop)
+		createGround(w, loop, ImgGroundName)
+		createTower(w, GamePosition{1, 3}, loop, ImgTowerName)
+		createChicken(w, GamePosition{0, 0}, loop)
+		loop++
+	}
 	// entities
-	createGround(w, 3, 5, 0, "grass-600-600.png")
+	//createGround(w, 0, ImgGroundName)
 	//createGround(w, 3, 5, 380, "stone-600-400.png")
 	//createGround(w, 3, 5, 780, "water-600-600.png")
-
-	createTower(w, 1, 3, 0, "tour1-600-600.png")
-	
-	createChicken(w)
 }
 
-func createTower(w *ecs.World, abs, ord float32, padding float32, imgName string) {
-	tower := createEntityTile(imgName, engo.Point{X: abs*TileWidth + padding, Y: ord * TileHeight})
+func createTower(w *ecs.World, p GamePosition, loop int, imgName string) {
+	tower := createEntityTower(imgName, p.toPoint(loop))
 	err := w.AddEntity(tower)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
-func createGround(w *ecs.World, width, length int, padding float32, imgName string) {
-	for j := 0; j < length; j++ {
-		for i := 0; i < width; i++ {
-			grass := createEntityTile(imgName, engo.Point{X: float32(i)*TileWidth + padding, Y: float32(j) * TileHeight})
+func createGround(w *ecs.World, loop int, imgName string) {
+	padding := loop * (nbTilesAbs*TileWidth + padRight)
+	for j := 0; j < nbTilesOrd; j++ {
+		for i := 0; i < nbTilesAbs; i++ {
+			grass := createEntityTile(imgName, engo.Point{X: float32(i*TileWidth + padding), Y: float32(j * TileHeight)})
 			err := w.AddEntity(grass)
 			if err != nil {
 				log.Println(err)
@@ -74,26 +97,25 @@ func createGround(w *ecs.World, width, length int, padding float32, imgName stri
 	}
 }
 
-func createChicken(w *ecs.World) {
+func createChicken(w *ecs.World, p GamePosition, loop int) {
 	spriteSheet := engo.NewSpritesheetFromFile("chicken.png", 150, 150)
-
-	err := w.AddEntity(createEntity(&engo.Point{X:0, Y:0}, spriteSheet, StopAction))
+	err := w.AddEntity(createEntityChicken(p.toPoint(loop), spriteSheet, system.StopAction))
 	if err != nil {
 		log.Println(err)
 	}
 }
 
-func createEntity(point *engo.Point, spriteSheet *engo.Spritesheet, action *engo.AnimationAction) *ecs.Entity {
+func createEntityChicken(point engo.Point, spriteSheet *engo.Spritesheet, action *engo.AnimationAction) *ecs.Entity {
 	entity := ecs.NewEntity("AnimationSystem", "RenderSystem", "ControlSystem")
 
-	
-	render := engo.NewRenderComponent(spriteSheet.Cell(action.Frames[0]), engo.Point{X:1, Y:1}, "chicken")
+	render := engo.NewRenderComponent(spriteSheet.Cell(action.Frames[0]), engo.Point{X: 1, Y: 1}, "chicken")
 	width := 150 * render.Scale().X
 	height := 150 * render.Scale().Y
-	space := &engo.SpaceComponent{Position:*point, Width:width, Height:height}
-	animation := engo.NewAnimationComponent(spriteSheet.Drawables(), animationRate)
-	animation.AddAnimationActions(actions)
+	space := &engo.SpaceComponent{Position: point, Width: width, Height: height}
+	animation := engo.NewAnimationComponent(spriteSheet.Drawables(), system.AnimationRate)
+	animation.AddAnimationActions(system.Actions)
 	animation.SelectAnimationByAction(action)
+
 	entity.AddComponent(render)
 	entity.AddComponent(space)
 	entity.AddComponent(animation)
@@ -102,51 +124,39 @@ func createEntity(point *engo.Point, spriteSheet *engo.Spritesheet, action *engo
 }
 
 func createEntityTile(imgName string, point engo.Point) *ecs.Entity {
-	// Create an entity part of the Render
-	entityTile := ecs.NewEntity("RenderSystem")
-	// Retrieve a texture
+	entity := ecs.NewEntity("RenderSystem")
+
 	texture := engo.Files.Image(imgName)
-	// renvoie nill si image pas chargée
 	if texture == nil {
 		log.Fatalf("image %s not loaded\n", imgName)
 	}
-
-	render := engo.NewRenderComponent(texture, engo.Point{X:0.2, Y:0.2}, "tile")
-
+	render := engo.NewRenderComponent(texture, engo.Point{X: 0.2, Y: 0.2}, "tile")
 	width := texture.Width() * render.Scale().X
 	height := texture.Height() * render.Scale().Y
-	space := &engo.SpaceComponent{Position:point, Width:width, Height:height}
+	space := &engo.SpaceComponent{Position: point, Width: width, Height: height}
 
-	entityTile.AddComponent(render)
-	entityTile.AddComponent(space)
+	entity.AddComponent(render)
+	entity.AddComponent(space)
 
-	return entityTile
+	return entity
 }
 
-type ControlSystem struct {
-	ecs.LinearSystem
-}
+func createEntityTower(imgName string, point engo.Point) *ecs.Entity {
+	entity := ecs.NewEntity("RenderSystem")
 
-func (*ControlSystem) Type() string { return "ControlSystem" }
-func (*ControlSystem) Pre()         {}
-func (*ControlSystem) Post()        {}
-
-func (c *ControlSystem) New(*ecs.World) {}
-
-func (c *ControlSystem) UpdateEntity(entity *ecs.Entity, dt float32) {
-	var a *engo.AnimationComponent
-
-	if !entity.Component(&a) {
-		return
+	texture := engo.Files.Image(imgName)
+	if texture == nil {
+		log.Fatalf("image %s not loaded\n", imgName)
 	}
+	render := engo.NewRenderComponent(texture, engo.Point{X: 0.2, Y: 0.2}, "tile")
+	width := texture.Width() * render.Scale().X
+	height := texture.Height() * render.Scale().Y
+	space := &engo.SpaceComponent{Position: point, Width: width, Height: height}
 
-	if engo.Keys.Get(engo.ArrowRight).Down() {
-		a.SelectAnimationByAction(WalkAction)
-	} else if engo.Keys.Get(engo.Space).Down() {
-		a.SelectAnimationByAction(DieAction)
-	} else {
-		a.SelectAnimationByAction(StopAction)
-	}
+	entity.AddComponent(render)
+	entity.AddComponent(space)
+
+	return entity
 }
 
 //Hide hide
@@ -166,14 +176,21 @@ func (*GameWorld) Exit() {
 func (*GameWorld) Type() string { return "GameWorld" }
 
 func main() {
-	opts := engo.RunOptions{
-		Title:  "Code of War : Enlarge your tower",
-		Width:  1024,
-		Height: 640,
+	players = make(map[string]*Player, 2)
+	players["1"] = &Player{Id: "1"}
+	players["2"] = &Player{Id: "2"}
+	sizeWidth := TileWidth * nbTilesAbs * len(players)
+	if sizeWidth > screenWidth {
+		padRight = 10
+	} else {
+		padRight = (screenWidth - sizeWidth) / len(players)
 	}
-	TileHeight= 120
-	TileWidth= 120
-	animationRate=0.1
+	opts := engo.RunOptions{
+		Title:      "Code of War : Enlarge your tower",
+		Width:      screenWidth,
+		Height:     screenHeight,
+		Fullscreen: false,
+	}
 	engo.OverrideCloseAction()
 	engo.Run(opts, &GameWorld{})
 }
